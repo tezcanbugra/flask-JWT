73052bb19978c0b0e353d8e872841a771a5dc399
tezcanbugra <bugra.tezcan2017@gtu.edu.tr>
2021-10-27 12:03:06 +0300

    syntax errors fixed

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, make_response
 import jwt, datetime
-import functools import wraps
+from functools import wraps
 
 
 app = Flask(__name__)
@@ -18,7 +18,7 @@ def token_required(f):
 
         try: 
             data = jwt.decode(token, app.config['SECRET_KEY'])
-        else: 
+        except: 
             return jsonify({'message' : 'Token is invalid.'}) , 403
 
         return f(*args, **kwargs)
@@ -29,17 +29,18 @@ def token_required(f):
 def unprotected():
     return jsonify({'message' : 'Anyone can see this.'})
 
-@app.route('protected')
+@app.route('/protected')
 @token_required
 def protected():
     return jsonify({'message' : 'This is a protected page.'})
 
-@app.route('login')
+@app.route('/login')
 def login():
     auth = request.authorization
 
-    if auth and auth.password == 'password'
-        token = jwt.encode({'user': auth.username, 'expire': datetime.datetime.utcnow() + datetime.timedelta(minutes=30), app.config['SECRET_KEY']})
+    if auth and auth.password == 'password':
+        token = jwt.encode({'user': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
         return jsonify({'token' : token.decode('UTF-8')})
 
     return make_response('Couldn\'t verify!', 401, {'WWW-Authenticate':'Basic realm="Login Required"'})
